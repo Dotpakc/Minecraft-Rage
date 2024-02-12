358a8f25bdc67e2e2a10f72bbe9d9c7ab1918bbd
DotPack <lifirenko123@gmail.com>
2024-02-12 22:30:47 +0200

    add skins, bootstrap,logo

--- apps/skins/views.py
+++ apps/skins/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from .models import Skin
+
 # Create your views here.
+def index(request):
+    skins = Skin.objects.all()
+    return render(request, 'skins/index.html', {'skins': skins})
--- coremine/urls.py
+++ coremine/urls.py
@@ -21,5 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('skins/', include('apps.skins.urls')),
     path('', include('apps.main.urls')),
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
