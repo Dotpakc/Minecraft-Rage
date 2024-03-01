7211248fcdbf95165a3c745d2c79e5b658d2373b
DotPack <lifirenko123@gmail.com>
2024-03-01 17:06:44 +0200

    Logins templates

--- apps/members/views.py
+++ apps/members/views.py
@@ -1,22 +1,26 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
 def login_view(request):
     if request.method == 'POST':
-        form = AuthenticationForm(request.POST)
-        print(form.is_valid())
+        form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
+            messages.success(request, 'Welcome back!')
             return redirect('members:profile')
     else:
         form = AuthenticationForm()
     return render(request, 'members/login.html', {'form': form})
 
+@login_required
 def logout_view(request):
     logout(request)
+    messages.info(request, 'You have been logged out.')
     return redirect('main:index')
 
 def signup(request):
@@ -25,11 +29,13 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
+            messages.success(request, f'Welcome, to the club, {user.username}!')
+            messages.add_message(request, messages.INFO, 'You have been registered successfully!')
             return redirect('members:profile')
     else:
         form = UserCreationForm()
     return render(request, 'members/signup.html', {'form': form})
 
-
+@login_required
 def profile(request):
     return render(request, 'members/profile.html')
