d6cbbd43a345695593589aa6330497f5817395fd
Lifirenko Yegor <lifirenko123@gmail.com>
2024-03-16 01:26:02 +0200

    optimizations

--- apps/skins/models.py
+++ apps/skins/models.py
@@ -1,3 +1,5 @@
+import os
+
 from uuid import uuid4
 from PIL import Image
 
@@ -7,10 +9,15 @@ from apps.members.models import Profile
 
 # Create your models here.
 
+def skin_image_filename(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = f"{instance.id}.{ext}"
+    return os.path.join('skins/images', filename)
+
 class Skin(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='skins/images')
+    image = models.ImageField(upload_to=skin_image_filename)
     image_head = models.ImageField(upload_to='skins/images/head/', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -28,30 +35,31 @@ class Skin(models.Model):
     
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
-        self.image_head = self.create_head()
-        super().save(*args, **kwargs)
+        if not self.image_head:  # Проверяем, не существует ли уже головы для этого скина
+            self.create_head()
+            super().save(*args, **kwargs)
     
     def create_head(self):
         img = Image.open(self.image.path).convert("RGBA")
         x, y = img.size
         standard_size = [(8, 8, 16, 16), (40, 8, 48, 16), 128]
-        head_coord = []
-        helm_coord = []
-        for i in standard_size[0]:
-            head_coord.append(i * (x // 64))
-        for i in standard_size[1]:
-            helm_coord.append(i * (x // 64))
+        head_coord = [i * (x // 64) for i in standard_size[0]]
+        helm_coord = [i * (x // 64) for i in standard_size[1]]
+        
         head = img.crop(head_coord)
         helm = img.crop(helm_coord)
         k = standard_size[2] // (x // 64)
+        
         x, y = head.size
         head.paste(helm, (0, 0, x, y), helm)
+        
         new_width = x * k
         new_height = y * k
-        resized_head = Image.new('RGBA', (new_width, new_height))
-        for i in range(x):
-            for j in range(y):
-                pixel = head.getpixel((i, j))
-                resized_head.paste(pixel, (i * k, j * k, (i + 1) * k, (j + 1) * k))
-        resized_head.save("media/skins/head/" + str(self.id) + ".png")
-        return "skins/head" + str(self.id) + ".png"
+        resized_head = head.resize((new_width, new_height), Image.ANTIALIAS)
+        
+        # Получаем путь для сохранения
+        head_filename = os.path.join('skins/images/head/', f"{self.id}.png")
+        resized_head.save(head_filename)
+        
+        # Присваиваем значение полю image_head
+        self.image_head.name = head_filename
